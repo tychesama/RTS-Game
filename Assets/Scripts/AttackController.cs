10fe497d277ae7f7fc0ab0a933f9b215686f292f
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-01-08 09:45:12 +0800

    unfinished feat: unit states

---
+++ Assets/Scripts/AttackController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    public Transform targetToAttack;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy") && targetToAttack == null)
+        {
+            targetToAttack = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Enemy") && targetToAttack != null)
+        {
+            targetToAttack = null;
+        }
+    }
+}
