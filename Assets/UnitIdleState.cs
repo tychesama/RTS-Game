ae67f10f06f152a12a554abdbed2e9f61505881e
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-01-08 09:51:43 +0800

    Update UnitIdleState.cs

--- Assets/UnitIdleState.cs
+++ Assets/UnitIdleState.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 public class UnitIdleState : StateMachineBehaviour
 {
+    AttackController attackController;
+
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+       attackController = animator.transform.GetComponent<AttackController>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
-    }
+       if (attackController.targetToAttack != null)
+        {
+            animator.SetBool("isFollowing", true);
+        }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-       
-    }
 
+    }
 }
